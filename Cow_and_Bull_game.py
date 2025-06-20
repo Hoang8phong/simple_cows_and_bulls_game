3ddf9355f97fc30a1ab41b0f056e7609dfdba83f
Brian Hoang <132343806+Hoang8phong@users.noreply.github.com>
2025-06-20 23:35:57 +1000

    Add files via upload

---
+++ Cow_and_Bull_game.py
@@ -0,0 +1,55 @@
+# Generate a secret number with unique digits
+# Loop
+#   Ask user to guess
+#   Validate the guess (4 digits - unique)
+#   If invalid
+#       print error
+#       Continue
+#   ELse
+#       Calculate cows and bulls
+#           for each digit in the guess
+#               if digit exists in the same position in the secret
+#                   Increment bulls
+#               if digits exists in the secret
+#                   Increment cows
+#       Print cows and bulls
+#       if bulls == 4
+#           Print message
+#           break
+
+import random
+
+def generate_secret():
+    digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    random.shuffle(digits)
+    secret = ''
+    for digit in digits[:4]:
+        secret += str(digit)
+    return secret
+
+def calculate_cows_and_bulls(secret, guess):
+    cows = 0
+    bulls = 0
+
+    for i in range(4):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+
+    return cows, bulls
+
+secret = generate_secret()
+while True:
+    guess = input('Guess: ')
+    if len(guess) == 4 and guess.isdigit() and len(set(guess)) == 4:
+        cows, bulls = calculate_cows_and_bulls(secret, guess)
+        print(f"{cows} cows, {bulls} bulls")
+
+        if bulls == 4:
+            print("You win!")
+            break
+    else:
+        print("Invalid guess.")
+
+
